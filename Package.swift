40ac8a481d28365115bc91a2904ef64e0ba7d528
Evgenia Gorbacheva <31763733+EvgeniaGorbacheva@users.noreply.github.com>
2026-09-22 15:53:06 +0200

    Update Package.swift
    Signed-off-by: Evgenia Gorbacheva <31763733+EvgeniaGorbacheva@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["AppodealYandexAdapterWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/appodeal/Appodeal-Swift-Package.git", .upToNextMajor(from: "4.0.0-alpha.1")),
+        .package(url: "https://github.com/appodeal/Appodeal-Swift-Package.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/yandexmobile/yandex-ads-sdk-ios", exact: "8.3.0"),
     ],
     targets: [
